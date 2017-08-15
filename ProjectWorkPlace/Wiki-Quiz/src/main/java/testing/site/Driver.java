7114a562cedae1a8a22b56a742744d1723a48a00
EStansell <30707699+EStansell@users.noreply.github.com>
2017-08-15 14:12:39 -0600

    Initial Project

---
+++ ProjectWorkPlace/Wiki-Quiz/src/main/java/testing/site/Driver.java
@@ -0,0 +1,17 @@
+package testing.site;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Driver d = new Driver();
+		d.run();
+	}
+	
+	public void run() {
+		
+	}
+	
+	
+	
+	
+}
